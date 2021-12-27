de65b4c54bde117405bba6c93cabd82dd4f9373f
AP-Studios <61108504+AP-Studios@users.noreply.github.com>
2021-12-27 16:46:03 +0100

    JColor.java

---
+++ JColor.java
@@ -0,0 +1,25 @@
+package org.aprompt.lib;
+
+/*
+* By AP-Studios
+* Copyright (c) 2021 AP-Studios
+*/
+
+public class JColor {					
+	public static final String RESET  = "\u001B[0m";
+	public static final String BLACK  = "\u001B[30m";
+	public static final String RED    = "\u001B[31m";
+	public static final String GREEN  = "\u001B[32m";
+	public static final String YELLOW = "\u001B[33m";
+	public static final String BLUE   = "\u001B[34m";
+	public static final String PURPLE = "\u001B[35m";
+	public static final String CYAN   = "\u001B[36m";
+	public static final String GREY   = "\u001B[37m";
+	public static final String WHITE  = "\u001B[97m";
+	public static final String LIGHT_RED    = "\u001B[91m";
+	public static final String LIGHT_GREEN  = "\u001B[92m";
+	public static final String LIGHT_YELLOW = "\u001B[93m";
+	public static final String LIGHT_BLUE   = "\u001B[94m";
+	public static final String LIGHT_PURPLE = "\u001B[95m";
+	public static final String LIGHT_CYAN   = "\u001B[96m";
+}
